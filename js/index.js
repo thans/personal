b01fa2e6fb33396d361a3488c927dec658f773f1
thans <tore@Tores-MacBook-Air.local>
2013-09-22 14:29:33 -0700

    initial version

---
+++ js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    global = {};
+    $('#nav').on("mouseover", expand);
+    $('#nav').on("mouseout", contract);
+
+    function expand() {
+        console.log('expanding');
+        $('#nav').animate({
+            height: '200px'
+        }, {
+            duration: 500,
+            queue: false
+        });
+        $('#nav').animate({
+            borderBottomRightRadius: '0px'
+        }, {
+            duration: 500,
+            queue: false
+        });
+
+                    
+    }
+
+    function contract() {
+        console.log('contracting');
+        $('#nav').animate({
+            height: '100px'
+        }, {
+            duration: 500,
+            queue: false
+        });
+        $('#nav').animate({
+            borderBottomRightRadius: '100px'
+        }, {
+            duration: 500,
+            queue: false
+        });
+    }
+});
